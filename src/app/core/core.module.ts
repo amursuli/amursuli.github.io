6dd1904f0da0a970c22f63ace573fc9db04c9d57
mursuli <P4ssw0rd>
2023-06-26 14:53:12 -0400

    [register services]

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LocalStorageService } from './services/local-storage/local-storage.service';
+import { TriviaQuizService } from './services/trivia-quiz/trivia-quiz.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  providers: [LocalStorageService, TriviaQuizService],
 })
 export class CoreModule {}
